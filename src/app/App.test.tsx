2485df31a34e1b178ba807225f5a531836d8f459
Matthew Vance <mavance44@gmail.com>
2020-08-16 09:59:35 -0400

    add test coverage to App

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,39 +1,71 @@
 import React from "react";
-import { render } from "test-utils";
+import { render, fireEvent, waitFor } from "test-utils";
 import App from ".";
-import { DataContext } from "../providers/DataProvider";
+import DataProvider, { DataContext } from "../providers/DataProvider";
+import { fetchData, Data } from "../api/dataApi";
+jest.mock("../api/dataApi");
+const mockFetchData = fetchData as jest.Mock<Promise<Data>>;
 
 test("displays posts", async () => {
-    const { getByText } = render(
-        <DataContext.Provider
-            value={{
-                users: [],
-                posts: [
-                    {
-                        id: 1,
-                        userId: 1,
-                        creationDate: new Date(),
-                        message: "test message 1",
-                        likes: 0,
-                        liked: false,
-                    },
-                    {
-                        id: 2,
-                        userId: 1,
-                        creationDate: new Date(),
-                        message: "test message 2",
-                        likes: 0,
-                        liked: false,
-                    },
-                ],
-                comments: [],
-                setPost: () => {},
-                setComment: () => {},
-            }}
-        >
-            <App />
-        </DataContext.Provider>
-    );
-    getByText("test message 1");
-    getByText("test message 2");
+  const { getByText } = render(
+    <DataContext.Provider
+      value={{
+        users: [],
+        posts: [
+          {
+            id: 1,
+            userId: 1,
+            creationDate: new Date(),
+            message: "test message 1",
+            likes: 0,
+            liked: false,
+          },
+          {
+            id: 2,
+            userId: 1,
+            creationDate: new Date(),
+            message: "test message 2",
+            likes: 0,
+            liked: false,
+          },
+        ],
+        comments: [],
+        setPost: () => {},
+        setComment: () => {},
+      }}
+    >
+      <App />
+    </DataContext.Provider>
+  );
+  getByText("test message 1");
+  getByText("test message 2");
+});
+
+test("clicking 'like' toggles liked status", async () => {
+  mockFetchData.mockResolvedValue({
+    users: [],
+    posts: [
+      {
+        id: 1,
+        userId: 1,
+        creationDate: new Date(),
+        message: "test message 1",
+        likes: 0,
+        liked: false,
+      },
+    ],
+    comments: [],
+  });
+  const { getByText } = render(
+    <DataProvider>
+      <App />
+    </DataProvider>
+  );
+
+  await waitFor(() => getByText("0 Likes"));
+  const likeButton = getByText("Like");
+  fireEvent.click(likeButton);
+  getByText("1 Like");
+  fireEvent.click(likeButton);
+  getByText("0 Likes");
 });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,48 +4,46 @@ import styled from "styled-components";
 import { useData } from "../providers/DataProvider";
 
 const StyledApp = styled.div`
-    max-width: ${({ theme }) => {
-        return theme.breakpoints.md;
-    }};
-    margin-left: auto;
-    margin-right: auto;
-    box-sizing: border-box;
+  max-width: ${({ theme }) => {
+    return theme.breakpoints.md;
+  }};
+  margin-left: auto;
+  margin-right: auto;
+  box-sizing: border-box;
 `;
 
 const PostContainer = styled.div`
-    margin-top: 1rem;
+  margin-top: 1rem;
 
-    @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
-        margin: 2rem 1rem 0 1rem;
-    }
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    margin: 2rem 1rem 0 1rem;
+  }
 `;
 
 const App = (): JSX.Element => {
-    const { posts, setPost } = useData();
+  const { posts, setPost } = useData();
 
-    const handleOnLike = (postId: number) => {
-        const post = posts.find(p => p.id === postId);
+  const handleOnLike = (postId: number) => {
+    const post = posts.find(p => p.id === postId)!;
 
-        if (post) {
-            setPost({
-                ...post,
-                liked: !post.liked,
-                likes: post.liked ? post.likes - 1 : post.likes + 1,
-            });
-        }
-    };
+    setPost({
+      ...post,
+      liked: !post.liked,
+      likes: post.liked ? post.likes - 1 : post.likes + 1,
+    });
+  };
 
-    return (
-        <StyledApp>
-            <FlexContainer direction="column">
-                {posts.map((post, index) => (
-                    <PostContainer key={index}>
-                        <PostCard post={post} onLike={handleOnLike} />
-                    </PostContainer>
-                ))}
-            </FlexContainer>
-        </StyledApp>
-    );
+  return (
+    <StyledApp>
+      <FlexContainer direction="column">
+        {posts.map((post, index) => (
+          <PostContainer key={index}>
+            <PostCard post={post} onLike={handleOnLike} />
+          </PostContainer>
+        ))}
+      </FlexContainer>
+    </StyledApp>
+  );
 };
 
 export default App;
